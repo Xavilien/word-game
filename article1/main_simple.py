01acf9d59b1befb3ad981044add89f2c02c64ed8
xavilien <xavilien@gmail.com>
2021-09-26 21:45:53 +0800

    Incorporated get_next_letter() into main() to simplify code

--- article1/main_simple.py
+++ article1/main_simple.py
@@ -22,38 +22,35 @@ def value(curr_word, player, players_turn):
     return v
 
 
-def get_next_letter(curr_word, player):
-    possible_letters, possible_words = get_possible(curr_word)
-
-    if not possible_letters:
-        print("You win!") if get_score(player, curr_word) == 1 else print("You lose:(")
-        return
-
-    if len(possible_words) == 1:
-        word = curr_word + possible_words[0]
-        print("The only possible word is", word, '\n')
-        print("You win!") if get_score(player, word) == 1 else print("You lose:(")
-        return
-
-    scores = [value(curr_word + letter, player, 0) for letter in possible_letters]
-    best_indices = [i for i in range(len(scores)) if scores[i] == 1]
-    best_letters = [possible_letters[i] for i in best_indices]
-
-    if not best_letters:
-        print("There are no winning letters")
-        print("Possible letters: ", ", ".join(possible_letters))
-        return possible_letters
-
-    print("Best letters to pick are:", ", ".join(best_letters))
-
-    return best_letters
-
-
 def main():
     while True:
         curr_word = input("Starting letters: ")
         player = (len(curr_word)+1) % 2
-        get_next_letter(curr_word, player)
+
+        possible_letters, possible_words = get_possible(curr_word)
+
+        if not possible_letters:
+            print("You win!") if get_score(player, curr_word) == 1 else print("You lose:(")
+            return
+
+        if len(possible_words) == 1:
+            word = curr_word + possible_words[0]
+            print("The only possible word is", word, '\n')
+            print("You win!") if get_score(player, word) == 1 else print("You lose:(")
+            return
+
+        scores = [value(curr_word + letter, player, 0) for letter in possible_letters]
+        best_indices = [i for i in range(len(scores)) if scores[i] == 1]
+        best_letters = [possible_letters[i] for i in best_indices]
+
+        if not best_letters:
+            print("There are no winning letters")
+            print("Possible letters: ", ", ".join(possible_letters))
+            return possible_letters
+
+        print("Best letters to pick are:", ", ".join(best_letters))
+
+        return best_letters
 
 
 if __name__ == '__main__':
