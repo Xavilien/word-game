58814ca30132953a26a9ff9c3501628f46426433
xavilien <xavilien@gmail.com>
2021-09-26 19:17:12 +0800

    Fixed score function and added print lines for gist

--- article1/helper_functions_simple.py
+++ article1/helper_functions_simple.py
@@ -11,6 +11,7 @@ def search_remaining_letters(subword):
     return list(map(lambda x: x[len(subword):], search(subword)))
 
 
+# Returns list of next possible letters along with possible words
 def get_possible(curr_word):
     possible_words = search_remaining_letters(curr_word)
     if len(possible_words) == 0 or possible_words[0] == "":
@@ -20,11 +21,25 @@ def get_possible(curr_word):
     return sorted(list(set(possible_letters))), possible_words
 
 
+# Returns whether the player wins if the word is formed
 def get_score(player, word):
     parity = len(word) % 2
-    score = parity & (player % 2)
+    score = parity ^ (player % 2)
     return score
 
 
-# print(search(dictionary, "epidemi"))
-# print(search_remaining_letters(dictionary, "epidemi"))
+print(search("epidemi"))
+# >>> ['epidemic', 'epidemical', 'epidemically', 'epidemicalness', 'epidemicity', 'epidemiographist', 'epidemiography',
+# 'epidemiological', 'epidemiologist', 'epidemiology']
+
+print(search_remaining_letters("epidemi"))
+# >>> ['c', 'cal', 'cally', 'calness', 'city', 'ographist', 'ography', 'ological', 'ologist', 'ology']
+
+print(get_possible("epidemi"))
+# >>> (['c', 'o'], ['c', 'cal', 'cally', 'calness', 'city', 'ographist', 'ography', 'ological', 'ologist', 'ology'])
+
+print(get_score(1, "epidemic"))
+# >>> 1
+
+print(get_score(2, "epidemic"))
+# >>> 0
